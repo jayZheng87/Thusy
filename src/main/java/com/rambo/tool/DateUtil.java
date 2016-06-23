7ba280dc8c2b456fdeaa632334b38669c5c739ef
jayZheng87 <liujunbingdea5@163.com>
2016-06-23 10:02:41 +0800

    日期工具类提交

---
+++ src/main/java/com/rambo/tool/DateUtil.java
@@ -0,0 +1,641 @@
+package com.rambo.tool;
+
+import org.apache.commons.lang.time.DateFormatUtils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * 日期工具类, 继承org.apache.commons.lang.time.DateUtils类
+ */
+public class DateUtil extends org.apache.commons.lang.time.DateUtils {
+
+    private static String[] parsePatterns = {"yyyy-MM-dd", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm",
+            "yyyy-MM-dd", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm"};
+
+    /**
+     * 得到当前日期字符串 格式（yyyy-MM-dd）
+     */
+    public static String getDate() {
+        return getDate("yyyy-MM-dd");
+    }
+
+    /**
+     * 得到当前日期字符串 格式（yyyy-MM-dd） pattern可以为："yyyy-MM-dd" "HH:mm:ss" "E"
+     */
+    public static String getDate(String pattern) {
+        return DateFormatUtils.format(new Date(), pattern);
+    }
+
+    /**
+     * 得到日期字符串 默认格式（yyyy-MM-dd） pattern可以为："yyyy-MM-dd" "HH:mm:ss" "E"
+     */
+    public static String formatDate(Date date, Object... pattern) {
+        String formatDate = null;
+        if (pattern != null && pattern.length > 0) {
+            formatDate = DateFormatUtils.format(date, pattern[0].toString());
+        } else {
+            formatDate = DateFormatUtils.format(date, "yyyy-MM-dd");
+        }
+        return formatDate;
+    }
+
+    /**
+     * 得到日期时间字符串，转换格式（yyyy-MM-dd HH:mm:ss）
+     */
+    public static String formatDateTime(Date date) {
+        return formatDate(date, "yyyy-MM-dd HH:mm:ss");
+    }
+
+    /**
+     * 得到当前时间字符串 格式（HH:mm:ss）
+     */
+    public static String getTime() {
+        return formatDate(new Date(), "HH:mm:ss");
+    }
+
+    /**
+     * 得到当前日期和时间字符串 格式（yyyy-MM-dd HH:mm:ss）
+     */
+    public static String getDateTime() {
+        return formatDate(new Date(), "yyyy-MM-dd HH:mm:ss");
+    }
+
+    /**
+     * 得到当前年份字符串 格式（yyyy）
+     */
+    public static String getYear() {
+        return formatDate(new Date(), "yyyy");
+    }
+
+    /**
+     * 得到上年年份字符串 格式（yyyy）
+     */
+    public static String getLastYear() {
+        Date years = addYears(new Date(), -1);
+        return formatDate(years, "yyyy");
+    }
+
+    /**
+     * 得到当前月份字符串 格式（MM）
+     */
+    public static String getMonth() {
+        return formatDate(new Date(), "MM");
+    }
+
+    /**
+     * 得到当前月份字符串 格式（MM）
+     */
+    public static String getLastMonth() {
+        Date months = addMonths(new Date(), -1);
+        return formatDate(months, "MM");
+    }
+
+    /**
+     * 得到当天字符串 格式（dd）
+     */
+    public static String getDay() {
+        return formatDate(new Date(), "dd");
+    }
+
+    /**
+     * 得到当前星期字符串 格式（E）星期几
+     */
+    public static String getWeek() {
+        return formatDate(new Date(), "E");
+    }
+
+    /**
+     * 日期型字符串转化为日期 格式
+     * { "yyyy-MM-dd", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm",
+     * "yyyy-MM-dd", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm" }
+     */
+    public static Date parseDate(Object str) {
+        if (str == null) {
+            return null;
+        }
+        try {
+            return parseDate(str.toString(), parsePatterns);
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+
+    /**
+     * 获取过去的天数
+     *
+     * @param date
+     * @return
+     */
+    public static long pastDays(Date date) {
+        long t = new Date().getTime() - date.getTime();
+        return t / (24 * 60 * 60 * 1000);
+    }
+
+    /**
+     * 获取过去的小时
+     *
+     * @param date
+     * @return
+     */
+    public static long pastHour(Date date) {
+        long t = new Date().getTime() - date.getTime();
+        return t / (60 * 60 * 1000);
+    }
+
+    /**
+     * 获取过去的分钟
+     *
+     * @param date
+     * @return
+     */
+    public static long pastMinutes(Date date) {
+        long t = new Date().getTime() - date.getTime();
+        return t / (60 * 1000);
+    }
+
+    /**
+     * 转换为时间（天,时:分:秒.毫秒）
+     *
+     * @param timeMillis
+     * @return
+     */
+    public static String formatDateTime(long timeMillis) {
+        long day = timeMillis / (24 * 60 * 60 * 1000);
+        long hour = (timeMillis / (60 * 60 * 1000) - day * 24);
+        long min = ((timeMillis / (60 * 1000)) - day * 24 * 60 - hour * 60);
+        long s = (timeMillis / 1000 - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60);
+        long sss = (timeMillis - day * 24 * 60 * 60 * 1000 - hour * 60 * 60 * 1000 - min * 60 * 1000 - s * 1000);
+        return (day > 0 ? day + "," : "") + hour + ":" + min + ":" + s + "." + sss;
+    }
+
+    /**
+     * 获取两个日期之间的天数
+     *
+     * @param before
+     * @param after
+     * @return
+     */
+    public static double getDistanceOfTwoDate(Date before, Date after) {
+        long beforeTime = before.getTime();
+        long afterTime = after.getTime();
+        return (afterTime - beforeTime) / (1000 * 60 * 60 * 24);
+    }
+
+    /**
+     * 得到某年某周的第一天
+     *
+     * @param year
+     * @param week
+     * @return
+     */
+    public static Date getFirstDayOfWeek(int year, int week) {
+        week = week - 1;
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, Calendar.JANUARY);
+        calendar.set(Calendar.DATE, 1);
+
+        Calendar cal = (Calendar) calendar.clone();
+        cal.add(Calendar.DATE, week * 7);
+
+        return getFirstDayOfWeek(cal.getTime());
+    }
+
+    /**
+     * 得到某年某周的最后一天
+     *
+     * @param year
+     * @param week
+     * @return
+     */
+    public static Date getLastDayOfWeek(int year, int week) {
+        week = week - 1;
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, Calendar.JANUARY);
+        calendar.set(Calendar.DATE, 1);
+        Calendar cal = (Calendar) calendar.clone();
+        cal.add(Calendar.DATE, week * 7);
+
+        return getLastDayOfWeek(cal.getTime());
+    }
+
+    /**
+     * 取得当前日期所在周的第一天
+     *
+     * @param date
+     * @return
+     */
+    public static Date getFirstDayOfWeek(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setFirstDayOfWeek(Calendar.SUNDAY);
+        calendar.setTime(date);
+        calendar.set(Calendar.DAY_OF_WEEK,
+                calendar.getFirstDayOfWeek()); // Sunday
+        return calendar.getTime();
+    }
+
+    /**
+     * 取得当前日期所在周的最后一天
+     *
+     * @param date
+     * @return
+     */
+    public static Date getLastDayOfWeek(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setFirstDayOfWeek(Calendar.SUNDAY);
+        calendar.setTime(date);
+        calendar.set(Calendar.DAY_OF_WEEK,
+                calendar.getFirstDayOfWeek() + 6); // Saturday
+        return calendar.getTime();
+    }
+
+    /**
+     * 取得当前日期所在周的前一周最后一天
+     *
+     * @param date
+     * @return
+     */
+    public static Date getLastDayOfLastWeek(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return getLastDayOfWeek(calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.WEEK_OF_YEAR) - 1);
+    }
+
+    /**
+     * 返回指定日期的月的第一天
+     *
+     * @param date
+     * @return
+     */
+    public static Date getFirstDayOfMonth(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH), 1);
+        return calendar.getTime();
+    }
+
+    /**
+     * Description:获取指定日期年的第一天
+     */
+    public static Date getFirstDayOfYear(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(calendar.get(Calendar.YEAR), Calendar.JANUARY, 1);
+        return calendar.getTime();
+    }
+
+    /**
+     * Description:获取指定日期年的最后一天
+     */
+    public static Date getLastDayOfYear(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(calendar.get(Calendar.YEAR), Calendar.DECEMBER, 31);
+        return calendar.getTime();
+    }
+
+    /**
+     * Description:获取指定日期年的第一天
+     */
+    public static Date getFirstDayOfLastYear(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(calendar.get(Calendar.YEAR) - 1, Calendar.JANUARY, 1);
+        return calendar.getTime();
+    }
+
+    /**
+     * Description:获取指定日期年的最后一天
+     */
+    public static Date getLastDayOfLastYear(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(calendar.get(Calendar.YEAR) - 1, Calendar.DECEMBER, 31);
+        return calendar.getTime();
+    }
+
+    /**
+     * 返回指定年月的月的第一天
+     *
+     * @param year
+     * @param month
+     * @return
+     */
+    public static Date getFirstDayOfMonth(Integer year, Integer month) {
+        Calendar calendar = Calendar.getInstance();
+        if (year == null) {
+            year = calendar.get(Calendar.YEAR);
+        }
+        if (month == null) {
+            month = calendar.get(Calendar.MONTH);
+        }
+        calendar.set(year, month, 1);
+        return calendar.getTime();
+    }
+
+    /**
+     * 返回指定日期的月的最后一天
+     *
+     * @param date
+     * @return
+     */
+    public static Date getLastDayOfMonth(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH), 1);
+        calendar.roll(Calendar.DATE, -1);
+        return calendar.getTime();
+    }
+
+    /**
+     * 返回指定年月的月的最后一天
+     *
+     * @param year
+     * @param month
+     * @return
+     */
+    public static Date getLastDayOfMonth(Integer year, Integer month) {
+        Calendar calendar = Calendar.getInstance();
+        if (year == null) {
+            year = calendar.get(Calendar.YEAR);
+        }
+        if (month == null) {
+            month = calendar.get(Calendar.MONTH);
+        }
+        calendar.set(year, month, 1);
+        calendar.roll(Calendar.DATE, -1);
+        return calendar.getTime();
+    }
+
+    /**
+     * Description:返回指定日期的上个月的第一天
+     */
+    public static Date getFirstDayOfLastMonth(Date date) {
+        Date addMonths = addMonths(date, -1);
+        return setDays(addMonths, 1);
+    }
+
+    /**
+     * 返回指定日期的上个月的最后一天
+     *
+     * @param date
+     * @return
+     */
+    public static Date getLastDayOfLastMonth(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) - 1, 1);
+        calendar.roll(Calendar.DATE, -1);
+        return calendar.getTime();
+    }
+
+    /**
+     * 返回指定日期的季的第一天
+     *
+     * @param date
+     * @return
+     */
+    public static Date getFirstDayOfQuarter(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return getFirstDayOfQuarter(calendar.get(Calendar.YEAR),
+                getQuarterOfYear(date));
+    }
+
+    /**
+     * 返回指定年季的季的第一天
+     *
+     * @param year
+     * @param quarter
+     * @return
+     */
+    public static Date getFirstDayOfQuarter(Integer year, Integer quarter) {
+        Calendar calendar = Calendar.getInstance();
+        Integer month;
+        if (quarter == 1) {
+            month = 1 - 1;
+        } else if (quarter == 2) {
+            month = 4 - 1;
+        } else if (quarter == 3) {
+            month = 7 - 1;
+        } else if (quarter == 4) {
+            month = 10 - 1;
+        } else {
+            month = calendar.get(Calendar.MONTH);
+        }
+        return getFirstDayOfMonth(year, month);
+    }
+
+    /**
+     * 返回当前日期的季的最后一天
+     *
+     * @param date
+     * @return
+     */
+    public static Date getLastDayOfQuarter(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return getLastDayOfQuarter(calendar.get(Calendar.YEAR),
+                getQuarterOfYear(date));
+    }
+
+    /**
+     * 返回指定年季的季的最后一天
+     *
+     * @param year
+     * @param quarter
+     * @return
+     */
+    public static Date getLastDayOfQuarter(Integer year, Integer quarter) {
+        Calendar calendar = Calendar.getInstance();
+        Integer month;
+        if (quarter == 1) {
+            month = 3 - 1;
+        } else if (quarter == 2) {
+            month = 6 - 1;
+        } else if (quarter == 3) {
+            month = 9 - 1;
+        } else if (quarter == 4) {
+            month = 12 - 1;
+        } else {
+            month = calendar.get(Calendar.MONTH);
+        }
+        return getLastDayOfMonth(year, month);
+    }
+
+    /**
+     * 返回当前日期的季的第一天
+     *
+     * @param date
+     * @return
+     */
+    public static Date getFirstDayOfLastQuarter(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return getFirstDayOfLastQuarter(calendar.get(Calendar.YEAR),
+                getQuarterOfYear(date));
+    }
+
+    /**
+     * 返回指定日期的上一季的最后一天
+     *
+     * @param date
+     * @return
+     */
+    public static Date getLastDayOfLastQuarter(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return getLastDayOfLastQuarter(calendar.get(Calendar.YEAR),
+                getQuarterOfYear(date));
+    }
+
+    /**
+     * 返回指定年季的上一季的最后一天
+     *
+     * @param year
+     * @param quarter
+     * @return
+     */
+    public static Date getLastDayOfLastQuarter(Integer year, Integer quarter) {
+        Calendar calendar = Calendar.getInstance();
+        Integer month = new Integer(0);
+        if (quarter == 1) {
+            month = 12 - 1;
+        } else if (quarter == 2) {
+            month = 3 - 1;
+        } else if (quarter == 3) {
+            month = 6 - 1;
+        } else if (quarter == 4) {
+            month = 9 - 1;
+        } else {
+            month = calendar.get(Calendar.MONTH);
+        }
+        return getLastDayOfMonth(year, month);
+    }
+
+    /**
+     * 返回指定年季的上一季的第一天
+     *
+     * @param year
+     * @param quarter
+     * @return
+     */
+    public static Date getFirstDayOfLastQuarter(Integer year, Integer quarter) {
+        Calendar calendar = Calendar.getInstance();
+        Integer month = new Integer(0);
+        if (quarter == 1) {
+            month = 12 - 3;
+        } else if (quarter == 2) {
+            month = 3 - 3;
+        } else if (quarter == 3) {
+            month = 6 - 3;
+        } else if (quarter == 4) {
+            month = 9 - 3;
+        } else {
+            month = calendar.get(Calendar.MONTH);
+        }
+        return getFirstDayOfMonth(year, month);
+    }
+
+    /**
+     * 返回指定日期的季度
+     *
+     * @param date
+     * @return
+     */
+    public static int getQuarterOfYear(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.MONTH) / 3 + 1;
+    }
+
+    /**
+     *Description:获取当前上一季度
+     */
+    public static int getLastQuarterOfYear(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.MONTH) / 3;
+    }
+
+
+    public static Date getDateStart(Date date) throws ParseException {
+        if (date == null) {
+            return null;
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        date = sdf.parse(formatDate(date, "yyyy-MM-dd") + " 00:00:00");
+        return date;
+    }
+
+    public static Date getDateEnd(Date date) throws ParseException {
+        if (date == null) {
+            return null;
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        date = sdf.parse(formatDate(date, "yyyy-MM-dd") + " 23:59:59");
+        return date;
+    }
+
+    /**
+     * 获取指定日期的星期
+     */
+    public static String getDateWeek(Date date) {
+        return formatDate(date, "E");
+    }
+
+
+    /**
+     * 获取某年某周的星期一
+     *
+     * @param y
+     * @param w
+     * @return
+     */
+    public static Date getBeginDate(Integer y, Integer w) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(y, 0, 1, 0, 0, 0);
+        int week = calendar.get(calendar.DAY_OF_WEEK) - 1;
+        int days = w * 7 - (8 - week);
+        calendar.add(Calendar.DATE, days - 1);
+        Date beginDate = calendar.getTime();
+        return beginDate;
+    }
+
+    /**
+     * 获取某年某周的星期天
+     *
+     * @param y
+     * @param w
+     * @return
+     */
+    public static Date getEndDate(Integer y, Integer w) {
+        Date beginDate = getBeginDate(y, w);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(beginDate);
+        calendar.add(Calendar.DATE, 6);
+        Date endDate = calendar.getTime();
+        return endDate;
+    }
+
+    /**
+     * Description:获取进行比较的日期整型数字
+     */
+    public static int getDateInt(String date) {
+        return Integer.parseInt(DateFormatUtils.format(DateUtil.parseDate(date), "yyyyMMdd"));
+    }
+
+    /**
+     * Description:获取进行比较的日期整型数字
+     */
+    public static int getDateInt(Date date) {
+        return Integer.parseInt(DateFormatUtils.format(date, "yyyyMMdd"));
+    }
+}
