39b061e6c111da2652111fc8eb97e509e57f76c5
jayZheng87 <liujunbingdea5@163.com>
2016-09-23 17:58:03 +0800

    first init

--- src/main/java/com/rambo/tools/ArrayUtil.java
+++ src/main/java/com/rambo/tools/ArrayUtil.java
@@ -1,18 +1,3 @@
-/**
- *  Copyright (c) 1997-2013, www.tinygroup.org (luo_guo@icloud.com).
- *
- *  Licensed under the GPL, Version 3.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *       http://www.gnu.org/licenses/gpl.html
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
 package com.rambo.tools;
 
 import java.util.List;
